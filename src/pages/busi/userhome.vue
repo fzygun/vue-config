e2cce24e4f7402f42ad70b6ce8f0b227ad9241b5
fzygun <fzygun@qq.com>
2017-08-29 09:07:28 +0800

    first commit

---
+++ src/pages/busi/userhome.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		{{msg}}
+	</div>
+</template>
+<script>
+	export default {
+		data(){
+			return {
+				msg:"hello"
+			}
+		},
+		mounted(){
+			
+		}
+	}
+</script>
+<style scoped>
+	div{
+		font-size:16rem;
+		text-align:center;
+	}
+</style>
